5cf591e333474aa9f2f28b0915438fae5daa1ede
Ewerton-Am <ewerton.amorim.si.ccna@gmail.com>
2025-09-21 16:57:31 -0300

    .

--- crud.py
+++ crud.py
@@ -1,3 +1,4 @@
+from logging import root
 import customtkinter as ctk
 from create import create_item
 from read import read_item
@@ -13,20 +14,17 @@ def menu_crud():
     root.geometry("300x400")
 
     # Buttons for options
-    button_create = ctk.CTkButton(root, text="Create", command=create_item(root))
+    button_create = ctk.CTkButton(root, text="Create", command=lambda: create_item(root))
     button_create.pack(pady=(10, 0))
 
-    button_read = ctk.CTkButton(root, text="Read", command=read_item(root))
+    button_read = ctk.CTkButton(root, text="Read", command=lambda: read_item(root))
     button_read.pack(pady=(10, 0))
 
-    button_update = ctk.CTkButton(root, text="Update", command=update_item(root))
+    button_update = ctk.CTkButton(root, text="Update", command=lambda: update_item(root))
     button_update.pack(pady=(10, 0))
 
-    button_delete = ctk.CTkButton(root, text="Delete", command=delete_item(root))
+    button_delete = ctk.CTkButton(root, text="Delete", command=lambda: delete_item(root))
     button_delete.pack(pady=(10, 0))
-    
-    button_exit = ctk.CTkButton(root, text="Exit", command=root.destroy)
-    button_exit.pack(pady=(10, 0))
 
     root.mainloop()
 
